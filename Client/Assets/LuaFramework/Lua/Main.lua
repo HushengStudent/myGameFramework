7372dcfbf337558f1a5553ca4cb9aabc7cf97bd4
胡胜同学 <husheng.student@foxmail.com>
2017-12-26 00:27:19 +0800

    add tolua

---
+++ Client/Assets/LuaFramework/Lua/Main.lua
@@ -0,0 +1,10 @@
+--主入口函数。从这里开始lua逻辑
+function Main()					
+	print("logic start")	 		
+end
+
+--场景切换通知
+function OnLevelWasLoaded(level)
+	collectgarbage("collect")
+	Time.timeSinceLevelLoad = 0
+end
