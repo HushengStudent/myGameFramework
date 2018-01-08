2e34a8d9c88e28eaec06b7fa393d53dd3258e1fc
胡胜同学 <husheng.student@foxmail.com>
2018-01-09 00:15:57 +0800

    add game

--- Client/Assets/LuaFramework/Lua/Game.lua
+++ Client/Assets/LuaFramework/Lua/Game.lua
@@ -2,3 +2,11 @@
 --- Created by husheng.
 --- DateTime: 2018/1/8 23:49
 ---
+
+require "Define"
+
+Game = {}
+
+Game.StartGame = function()
+    log("game start!")
+end
--- Client/Assets/LuaFramework/Lua/Main.lua
+++ Client/Assets/LuaFramework/Lua/Main.lua
@@ -1,8 +1,10 @@
 require "Define"
+require "Game"
 
 --主入口函数。从这里开始lua逻辑
 function Main()					
 	log("logic start")
+	Game.StartGame()
 end
 
 --场景切换通知
