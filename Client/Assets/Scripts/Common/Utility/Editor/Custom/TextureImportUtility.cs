8d5ecb7f56125fd3481b238ac2c928179683df62
胡胜同学 <husheng.student@foxmail.com>
2018-02-05 00:50:52 +0800

    add texture utility

---
+++ Client/Assets/Scripts/Common/Utility/Editor/Custom/TextureImportUtility.cs
@@ -0,0 +1,30 @@
+/********************************************************************************
+** auth:  https://github.com/HushengStudent
+** date:  2018/02/05 00:33:42
+** desc:  Œ∆¿Ì∏Ò Ω
+*********************************************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Framework
+{
+    public class TextureImportUtility
+    {
+        /*
+
+        private string path;
+
+        private void Test()
+        {
+            Texture2D activaeTex = Selection.activeObject as Texture2D;
+            TextureImporter importer = AssetImporter.GetAtPath(path) as TextureImporter;
+            //TODO
+            AssetDatabase.ImportAsset(path);
+            
+        }
+        
+        */
+    }
+}
