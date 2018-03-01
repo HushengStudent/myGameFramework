325d8d8dcd6f936d5ae298658c6b8739ce47557b
胡胜同学 <husheng.student@foxmail.com>
2018-03-02 00:59:44 +0800

    update pool

--- Client/Assets/Scripts/Framework/ObjectPool/GameObjectPool.cs
+++ Client/Assets/Scripts/Framework/ObjectPool/GameObjectPool.cs
@@ -1,25 +1,97 @@
 /********************************************************************************
 ** auth:  https://github.com/HushengStudent
 ** date:  2018/03/01 23:14:44
-** desc:  Unity¶ÔÏó³Ø
+** desc:  Unityå¯¹è±¡æ± 
 *********************************************************************************/
 
+using MEC;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameObjectPool : MonoBehaviour
+namespace Framework
 {
-
-    // Use this for initialization
-    void Start()
+    public class GameObjectPool
     {
+        private Dictionary<AssetType, Dictionary<string, Stack<GameObject>>> _pool =
+            new Dictionary<AssetType, Dictionary<string, Stack<GameObject>>>();
 
-    }
+        public GameObject Get(AssetType type, string assetName)
+        {
+            GameObject element;
+            Dictionary<string, Stack<GameObject>> m_Dict;
+            Stack<GameObject> m_Stack;
+            if (!_pool.TryGetValue(type, out m_Dict))
+            {
+                m_Dict = new Dictionary<string, Stack<GameObject>>();
+                _pool[type] = m_Dict;
+            }
+            if (!m_Dict.TryGetValue(assetName, out m_Stack))
+            {
+                m_Stack = new Stack<GameObject>();
+                m_Dict[assetName] = m_Stack;
+            }
+            if (m_Stack.Count == 0)
+            {
+                element = null;
+            }
+            else
+            {
+                element = m_Stack.Pop();
+            }
+            return element;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        public void Release(AssetType type, string assetName, GameObject element)
+        {
+            Dictionary<string, Stack<GameObject>> m_Dict;
+            Stack<GameObject> m_Stack;
+            if (!_pool.TryGetValue(type, out m_Dict))
+            {
+                m_Dict = new Dictionary<string, Stack<GameObject>>();
+                _pool[type] = m_Dict;
+            }
+            if (!m_Dict.TryGetValue(assetName, out m_Stack))
+            {
+                m_Stack = new Stack<GameObject>();
+                m_Dict[assetName] = m_Stack;
+            }
+            m_Stack.Push(element);
+        }
 
+        public IEnumerator<float> ClearPool(Action onFinish)
+        {
+            Dictionary<AssetType, Dictionary<string, Stack<GameObject>>> tempDict
+                = new Dictionary<AssetType, Dictionary<string, Stack<GameObject>>>(_pool);
+            _pool = new Dictionary<AssetType, Dictionary<string, Stack<GameObject>>>();
+
+            foreach (var temp in tempDict)
+            {
+                if (temp.Value == null)
+                {
+                    continue;
+                }
+                foreach (var value in temp.Value)
+                {
+                    Stack<GameObject> stack = value.Value;
+                    if (stack == null)
+                    {
+                        continue;
+                    }
+                    while (stack.Count > 0)
+                    {
+                        GameObject go = stack.Pop();
+                        GameObject.DestroyImmediate(go);
+                        yield return Timing.WaitForOneFrame;
+                    }
+                }
+            }
+            Resources.UnloadUnusedAssets();
+            if (onFinish != null)
+            {
+                onFinish();
+            }
+        }
     }
 }
--- Client/Assets/Scripts/Framework/ObjectPool/PoolMgr.cs
+++ Client/Assets/Scripts/Framework/ObjectPool/PoolMgr.cs
@@ -7,12 +7,22 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
+using Object = System.Object;
 
 namespace Framework
 {
     public class PoolMgr : Singleton<PoolMgr>
     {
+        /// <summary>
+        /// C# Object Pool;
+        /// </summary>
         private Dictionary<Type, Object> _pool = new Dictionary<Type, Object>();
+
+        /// <summary>
+        /// GameObject Pool;
+        /// </summary>
+        private GameObjectPool _gameObjectPool = new GameObjectPool();
 
         /// <summary>
         /// 获取对象池目标组件;
@@ -59,14 +69,6 @@ namespace Framework
         }
 
         /// <summary>
-        /// 销毁对象池;
-        /// </summary>
-        public void ClearPool()
-        {
-            _pool.Clear();
-        }
-
-        /// <summary>
         /// 创建对象池;
         /// </summary>
         /// <typeparam name="T"></typeparam>
@@ -84,6 +86,46 @@ namespace Framework
                 _pool[typeof(T)] = pool;
                 return pool;
             }
+        }
+
+        /// <summary>
+        /// 获取GameObject,可能为null;
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="assetName"></param>
+        /// <returns></returns>
+        public GameObject Get(AssetType type, string assetName)
+        {
+            return _gameObjectPool.Get(type, assetName);
+        }
+
+        /// <summary>
+        /// 贮存GameObject;
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="assetName"></param>
+        /// <param name="element"></param>
+        public void Release(AssetType type, string assetName, GameObject element)
+        {
+            _gameObjectPool.Release(type, assetName, element);
+        }
+
+        /// <summary>
+        /// 销毁对象池;
+        /// </summary>
+        public void ClearPool(Action onFinish)
+        {
+            _pool.Clear();
+            CoroutineMgr.Instance.RunCoroutine(_gameObjectPool.ClearPool(
+                () =>
+                {
+                    System.GC.Collect();
+                    if (onFinish != null)
+                    {
+                        onFinish();
+                    }
+                }
+                ));
         }
     }
 }
