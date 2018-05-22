23ee900106bf6ca21d105e41deefaccdd0bfe88f
胡胜同学 <husheng.student@foxmail.com>
2018-05-23 00:55:14 +0800

    update update

---
+++ Client/Assets/Scripts/Framework/Update/UpdateMgr.cs
@@ -0,0 +1,17 @@
+/********************************************************************************
+** auth:  https://github.com/HushengStudent
+** date:  2018/05/23 00:53:17
+** desc:  热更管理;
+*********************************************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Framework
+{
+    public class UpdateMgr : Singleton<UpdateMgr>
+    {
+
+    }
+}
