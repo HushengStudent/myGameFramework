f6e8ae68caf8b48f96d8641c2e9f47c8ecdeb687
胡胜同学 <husheng.student@foxmail.com>
2018-06-17 02:29:42 +0800

    update navigation

--- Client/Assets/Test/NavigationTest.cs
+++ Client/Assets/Test/NavigationTest.cs
@@ -22,4 +22,21 @@ public class NavigationTest : MonoBehaviour
                 agent.SetDestination(hit.point);
         }
     }
+
+    private void OnDrawGizmos()
+    {
+        if (Application.isPlaying)
+        {
+            NavMeshPath path = agent.path;
+            Gizmos.color = Color.red;
+            if (path.corners.Length < 2)
+            {
+                return;
+            }
+            for (int i = 1; i < path.corners.Length; i++)
+            {
+                Gizmos.DrawLine(path.corners[i - 1], path.corners[i]);
+            }
+        }
+    }
 }
