3de917e08bbfabacf0f677ea709fcfdfea635c20
胡胜同学 <husheng.student@foxmail.com>
2018-06-29 01:57:05 +0800

    fix a bug

--- Client/Assets/Scripts/Framework/BehaviorTree/BehaviorTreeFactory.cs
+++ Client/Assets/Scripts/Framework/BehaviorTree/BehaviorTreeFactory.cs
@@ -4,6 +4,7 @@
 ** desc:  行为树工厂;
 *********************************************************************************/
 
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -15,7 +16,7 @@ namespace Framework
         private static Dictionary<int, AbsBehavior> _behaviorDict = new Dictionary<int, AbsBehavior>();
         private static Dictionary<int, List<int>> _connectionDict = new Dictionary<int, List<int>>();
 
-        public static BehaviorTree CreateBehaviorTree(BaseEntity entity,string path)
+        public static BehaviorTree CreateBehaviorTree(BaseEntity entity, string path)
         {
             ReadConfig(path);
             BehaviorTree tree = new BehaviorTree(null, entity);
@@ -27,7 +28,34 @@ namespace Framework
             _behaviorDict.Clear();
             _connectionDict.Clear();
             TextAsset json = Resources.Load<TextAsset>(path);
-            ArrayList list = MiniJsonExtensions.arrayListFromJson(json.text);
+            string content = json.text.Replace("\r", "").Replace("\n", "");
+            Hashtable table = MiniJsonExtensions.hashtableFromJson(content);
+            object nodes = table["nodes"];
+        }
+
+        private static AbsBehavior CreateBehavior(string type)
+        {
+            AbsBehavior behavior = null;
+            string[] str = type.Split(".".ToCharArray());
+            if (str.Length < 3)
+            {
+                LogUtil.LogUtility.PrintError(string.Format("[BehaviorTreeFactory]create Behavior error,type is {0}", type));
+                return behavior;
+            }
+            switch (str[2])
+            {
+                case "BTLog":
+                    break;
+                case "BTSequence":
+                    break;
+                case "BTRandom":
+                    break;
+                case "BTSelector":
+                    break;
+                default:
+                    break;
+            }
+            return behavior;
         }
     }
 }
