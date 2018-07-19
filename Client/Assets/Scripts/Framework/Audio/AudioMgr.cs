c12063e2b77da05b987272a9fe9bbf03b7a6dfa5
胡胜同学 <husheng.student@foxmail.com>
2018-07-19 23:48:21 +0800

    update mgr

---
+++ Client/Assets/Scripts/Framework/Audio/AudioMgr.cs
@@ -0,0 +1,17 @@
+/********************************************************************************
+** auth:  https://github.com/HushengStudent
+** date:  2018/07/19 23:42:33
+** desc:  音频管理;
+*********************************************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Framework
+{
+    public class AudioMgr : Singleton<AudioMgr>
+    {
+
+    }
+}
---
+++ Client/Assets/Scripts/Framework/Timeline/TimelineMgr.cs
@@ -0,0 +1,17 @@
+/********************************************************************************
+** auth:  https://github.com/HushengStudent
+** date:  2018/07/19 23:40:57
+** desc:  Timeline管理;
+*********************************************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Framework
+{
+    public class TimelineMgr : Singleton<TimelineMgr>
+    {
+
+    }
+}
