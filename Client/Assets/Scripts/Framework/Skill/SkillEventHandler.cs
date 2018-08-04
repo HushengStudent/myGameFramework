d60334679d8fa52533fe6d6488fb8a284e305f72
胡胜同学 <husheng.student@foxmail.com>
2018-08-05 02:48:02 +0800

    add handler

---
+++ Client/Assets/Scripts/Framework/Skill/SkillEventHandler.cs
@@ -0,0 +1,15 @@
+/********************************************************************************
+** auth:  https://github.com/HushengStudent
+** date:  2018/08/05 02:45:19
+** desc:  ¼¼ÄÜÎ¯ÍÐ;
+*********************************************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Framework
+{
+    //public delegate void SkillEventHandler();
+}
+
