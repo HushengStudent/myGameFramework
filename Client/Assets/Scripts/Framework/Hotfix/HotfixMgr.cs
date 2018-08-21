747e358d338687a4a85786a48c31e140a4f1a6a2
胡胜同学 <husheng.student@foxmail.com>
2018-08-22 01:08:29 +0800

    add hotfixmgr

---
+++ Client/Assets/Scripts/Framework/Hotfix/HotfixMgr.cs
@@ -0,0 +1,18 @@
+/********************************************************************************
+** auth:  https://github.com/HushengStudent
+** date:  2018/08/22 01:06:32
+** desc:  cs热更管理;
+*********************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Framework
+{
+    public class HotfixMgr
+    {
+
+    }
+}
