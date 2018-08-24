3e7bcda2dc7f4236ad12e2333447628960f04a01
胡胜同学 <husheng.student@foxmail.com>
2018-08-25 01:36:03 +0800

    add InfoComponent

---
+++ Client/Assets/Scripts/Framework/Component/Info/InfoComponent.cs
@@ -0,0 +1,17 @@
+/********************************************************************************
+** auth:  https://github.com/HushengStudent
+** date:  2018/08/25 01:28:00
+** desc:  InfoComponent;
+*********************************************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Framework
+{
+    public class InfoComponent : AbsComponent
+    {
+
+    }
+}
