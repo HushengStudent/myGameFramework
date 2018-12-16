4172ed96c9730353e5813d0ef6f01c2ecee5edb0
胡胜同学 <husheng.student@foxmail.com>
2018-12-16 21:55:32 +0800

    fix a bug

--- Tools/LogHelper/LogHelper/Properties/AssemblyInfo.cs
+++ Tools/LogHelper/LogHelper/Properties/AssemblyInfo.cs
@@ -14,6 +14,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+[assembly: UnityEngine.UnityAPICompatibilityVersionAttribute("2017.4.16f1")]
+
 // 将 ComVisible 设置为 false 会使此程序集中的类型
 //对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型
 //请将此类型的 ComVisible 特性设置为 true。
