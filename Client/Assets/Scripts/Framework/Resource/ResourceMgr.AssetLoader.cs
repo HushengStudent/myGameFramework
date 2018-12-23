c13a711351dd105c05da3e1bfec1a233cdfc3b80
胡胜同学 <husheng.student@foxmail.com>
2018-12-23 17:40:11 +0800

    rename

---
+++ Client/Assets/Scripts/Framework/Resource/ResourceMgr.AssetLoader.cs
@@ -0,0 +1,19 @@
+/********************************************************************************
+** auth:  https://github.com/HushengStudent
+** date:  2018/12/23 17:34:50
+** desc:  ×ÊÔ´¼ÓÔØÆ÷;
+*********************************************************************************/
+
+using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using MEC;
+using Object = UnityEngine.Object;
+
+namespace Framework
+{
+    public partial class ResourceMgr
+    {
+    }
+}
