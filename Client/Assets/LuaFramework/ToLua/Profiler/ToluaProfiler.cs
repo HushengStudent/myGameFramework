332943fc42951586637adb4e4dfe378fb84902d4
胡胜同学 <husheng.student@foxmail.com>
2019-05-12 16:43:23 +0800

    add lua profiler

---
+++ Client/Assets/LuaFramework/ToLua/Profiler/ToluaProfiler.cs
@@ -0,0 +1,34 @@
+﻿/********************************************************************************
+** auth:  https://github.com/HushengStudent
+** date:  2019/05/12 00:03:35
+** desc:  tolua profiler;
+*********************************************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ToluaProfiler
+{
+    private static Dictionary<string, ulong> _luaCallCountDict = new Dictionary<string, ulong>();
+
+    private static Dictionary<string, ulong> _frameCallCountDict = new Dictionary<string, ulong>();
+
+    public static void AddCallRecord(string name)
+    {
+#if UNITY_EDITOR
+        //if (_luaCallCountDict.TryGetValue(name, out var count))
+        {
+
+        }
+#endif
+    }
+
+    public static void Update()
+    {
+#if UNITY_EDITOR
+
+#endif
+    }
+
+}
