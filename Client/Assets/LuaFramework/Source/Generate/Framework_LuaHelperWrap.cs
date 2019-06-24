907c1a1e16cf09667fb6b49a068daa3b55b0909a
胡胜同学 <husheng.student@foxmail.com>
2019-06-24 23:47:41 +0800

    update wrap

--- Client/Assets/LuaFramework/Source/Generate/Framework_LuaHelperWrap.cs
+++ Client/Assets/LuaFramework/Source/Generate/Framework_LuaHelperWrap.cs
@@ -11,6 +11,8 @@ public class Framework_LuaHelperWrap
 		L.RegFunction("Int", Int);
 		L.RegFunction("SetLocalPosition", SetLocalPosition);
 		L.RegFunction("SetTransParent", SetTransParent);
+		L.RegFunction("IsNull", IsNull);
+		L.RegFunction("WorldToScreenPoint", WorldToScreenPoint);
 		L.EndStaticLibs();
 	}
 
@@ -87,6 +89,46 @@ public class Framework_LuaHelperWrap
 			UnityEngine.Transform arg1 = (UnityEngine.Transform)ToLua.CheckObject<UnityEngine.Transform>(L, 2);
 			Framework.LuaHelper.SetTransParent(arg0, arg1);
 			return 0;
+		}
+		catch (Exception e)
+		{
+			return LuaDLL.toluaL_exception(L, e);
+		}
+	}
+
+	[MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+	static int IsNull(IntPtr L)
+	{
+#if UNITY_EDITOR
+        ToluaProfiler.AddCallRecord("Framework.LuaHelper.IsNull");
+#endif
+		try
+		{
+			ToLua.CheckArgsCount(L, 1);
+			object arg0 = ToLua.ToVarObject(L, 1);
+			bool o = Framework.LuaHelper.IsNull(arg0);
+			LuaDLL.lua_pushboolean(L, o);
+			return 1;
+		}
+		catch (Exception e)
+		{
+			return LuaDLL.toluaL_exception(L, e);
+		}
+	}
+
+	[MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+	static int WorldToScreenPoint(IntPtr L)
+	{
+#if UNITY_EDITOR
+        ToluaProfiler.AddCallRecord("Framework.LuaHelper.WorldToScreenPoint");
+#endif
+		try
+		{
+			ToLua.CheckArgsCount(L, 1);
+			UnityEngine.Vector3 arg0 = ToLua.ToVector3(L, 1);
+			UnityEngine.Vector2 o = Framework.LuaHelper.WorldToScreenPoint(arg0);
+			ToLua.Push(L, o);
+			return 1;
 		}
 		catch (Exception e)
 		{
