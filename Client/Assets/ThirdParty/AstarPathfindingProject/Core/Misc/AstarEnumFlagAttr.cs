d4e61c9eaf93ff08ab676e261c2289e7156d5f5f
胡胜同学 <husheng.student@foxmail.com>
2019-08-03 16:02:02 +0800

    review

--- Client/Assets/ThirdParty/AstarPathfindingProject/Core/Misc/AstarEnumFlagAttr.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-namespace Pathfinding {
-	/** \author http://wiki.unity3d.com/index.php/EnumFlagPropertyDrawer */
-	public class AstarEnumFlagAttribute : PropertyAttribute {}
-}
